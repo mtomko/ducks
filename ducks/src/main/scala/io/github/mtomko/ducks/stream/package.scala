019c95581d38a3c483762af60e046afe4ee01741
Mark Tomko <mark.tomko.1@gmail.com>
2019-11-28 21:54:20 -0700

    cleanup

--- ducks/src/main/scala/io/github/mtomko/ducks/stream/package.scala
+++ ducks/src/main/scala/io/github/mtomko/ducks/stream/package.scala
@@ -22,22 +22,22 @@ package object stream {
   def fastq[F[_]]: Pipe[F, String, Fastq] =
     _.chunkN(4, allowFewer = false).map(seg => Fastq(seg(0), seg(1), seg(2), seg(3)))
 
-  // from https://gist.github.com/kiambogo/8247a7bbf79f00414d1489b7e6fc90d0 and
+  // from
+  // https://gist.github.com/kiambogo/8247a7bbf79f00414d1489b7e6fc90d0
+  // and
   // https://gist.github.com/SystemFw/168ff694eecf45a8d0b93ce7ef060cfd
-  def groupBy[F[_], A, K](selector: A => F[K])(implicit F: Concurrent[F]): Pipe[F, A, (K, Stream[F, A])] = { in =>
-    Stream.eval(Ref.of[F, Map[K, Queue[F, Option[A]]]](Map.empty)).flatMap { st =>
-      val cleanup = {
-        st.get.flatMap(_.toList.traverse_(_._2.enqueue1(None)))
-      }
+  def groupBy[F[_], A, K](groupByF: A => F[K])(implicit F: Concurrent[F]): Pipe[F, A, (K, Stream[F, A])] = { in =>
+    Stream.eval(Ref.of[F, Map[K, Queue[F, Option[A]]]](Map.empty)).flatMap { ref =>
+      val cleanup: F[Unit] = ref.get.flatMap(_.toList.traverse_(_._2.enqueue1(None)))
 
       (in ++ Stream.eval_(cleanup)).evalMap { el =>
-        (selector(el), st.get).mapN { (key, queues) =>
+        (groupByF(el), ref.get).mapN { (key, queues) =>
           queues
             .get(key)
             .fold {
               for {
                 newQ <- Queue.unbounded[F, Option[A]] // Create a new queue
-                _ <- st.modify(x => (x + (key -> newQ), x)) // Update the ref of queues
+                _ <- ref.modify(x => (x + (key -> newQ), x)) // Update the ref of queues
                 _ <- newQ.enqueue1(el.some)
               } yield (key -> newQ.dequeue.unNoneTerminate).some
             }(_.enqueue1(el.some).as(None))
